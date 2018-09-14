056c167ae9777197a4e6a23d942af2b4125c4fa7
zhoujun1 <hayashihuu@yahoo.co.jp>
2018-09-14 17:16:44 +0800

    [FIX]启动熔断器

--- springCloudTest02/service-feign/src/main/java/com/forezp/service/SchedualServiceHiHystric.java
+++ springCloudTest02/service-feign/src/main/java/com/forezp/service/SchedualServiceHiHystric.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class SchedualServiceHiHystric implements SchedualServiceHi {
 
-
     @Override
     public String sayHiFromClientOne(String name) {
         return "sorry "+name;
